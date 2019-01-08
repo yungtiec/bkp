0873f4f0e03707ef17f46711ba9a43104906b3b4
yungtiec <tctammychu@gmail.com>
2019-01-08 16:05:30 +0800

    seed tags for staging

--- server/db/seeders/20181220041213-tags.js
+++ server/db/seeders/20181220041213-tags.js
@@ -9,7 +9,9 @@ module.exports = {
     const cities = await rp.get(
       "https://pkgstore.datahub.io/core/world-cities/world-cities_csv/data/6cc66692f0e82b18216a48443b6b95da/world-cities_csv.csv"
     );
-    const citiesArray = await parsePromise(cities, { columns: true });
+    const citiesArray = await parsePromise(cities.slice(0, 100), {
+      columns: true
+    });
     return queryInterface.bulkInsert(
       "tags",
       citiesArray
