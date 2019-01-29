0579eb940d5a8b8dba1b8c9e1db5da211e3a0f14
yungtiec <tctammychu@gmail.com>
2019-01-29 12:34:23 +0800

    set up landing page

--- client/scenes/landing/Landing.js
+++ client/scenes/landing/Landing.js
@@ -8,71 +8,25 @@ const Landing = ({ user, logout }) => (
   <div className="landing">
     <header className="masthead">
       <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
-        <div className="container">
+        <div className="container d-flex justify-content-center mt-5">
           <a
             className="navbar-brand js-scroll-trigger"
             href="https://thebkp.com/"
           />
-          <button
-            className="navbar-toggler navbar-toggler-right d-none"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            Menu
-            <i className="fa fa-bars" />
-          </button>
-
-          <div>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                {!!user.id ? (
-                  <a onClick={logout}>Log Out</a>
-                ) : (
-                  <Link
-                    to={{
-                      pathname: "/login",
-                      state: { lastPath: "/project/BKP/document/1/version/2" }
-                    }}
-                  >
-                    Log In
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
-
       <div className="container h-fill">
         <div className="row">
           <div className="col-lg-12 my-auto">
-            <div className="header-content mx-auto">
-              <h1 class="mb-5">
-                A collaborative approach to support innovators and protect
-                consumers on blockchains
-              </h1>
-              <div className="row">
-                <div className="col-lg-12 header-text">
-                  <p>
-                    The Brooklyn Project is developing a Core Framework and
-                    experimenting with a crypto-economic system to help consumer
-                    token projects engage the community to build more trust,
-                    cooperation, fairness, safety, and regulatory compliance
-                    across the industry.
-                  </p>
-                </div>
-              </div>
+            <div className="header-content mx-auto d-flex flex-column justify-content-center align-items-center">
+              <img
+                className="header-content__hero-img mb-5"
+                src="/assets/the-brooklyn-project-logo-white-transparent.png"
+              />
               <div className="row">
                 <div className="col-lg-12 my-auto">
-                  <Link
-                    class="btn btn-landing btn-xl"
-                    to="project/BKP/document/1/version/2"
-                  >
-                    Collaborate on Framework
+                  <Link class="btn btn-landing btn-xl" to="/feed">
+                    commnunity collaborations
                   </Link>
                   <a
                     class="btn btn-landing btn-xl ml-2"
@@ -80,7 +34,7 @@ const Landing = ({ user, logout }) => (
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    View pdf
+                    get involved
                   </a>
                 </div>
               </div>
@@ -89,72 +43,188 @@ const Landing = ({ user, logout }) => (
         </div>
       </div>
     </header>
-    <section class="framework" id="framework">
-      <div class="container">
-        <div class="row mb-3">
-          <div class="col-md-12">
-            <h2>The Core Framework consists of:</h2>
+    <section className="open-letter" id="open-letter">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-12 col-md-8 col-lg-8 col-xl-8">
+                <h1 className="py-3">
+                  An industry-wide initiative to promote token-powered economic
+                  growth and consumer protection.
+                </h1>
+              </div>
+              <div className="col-4" />
+            </div>
+            <div className="row">
+              <div className="col-1" />
+              <div className="col-12 col-md-7 col-lg-7 col-xl-7">
+                <figure>
+                  <blockquote className="py-3">
+                    <span>“</span>We are taking this action now, because we, as
+                    a company and an industry, have a unique opportunity to
+                    restore trust between people and institutions. By acting
+                    responsibly today, we can help make sure we are collectively
+                    able to reap the benefits of this powerful technology
+                    tomorrow.<span>”</span>
+                  </blockquote>
+                  <figcaption class="source">
+                    — Joseph Lubin, Founder of ConsenSys &amp; Ethereum
+                    Co-Founder
+                  </figcaption>
+                </figure>
+              </div>
+              <div className="col-4" />
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <p>
+                  Read the{" "}
+                  <a
+                    href="https://media.consensys.net/announcing-the-brooklyn-project-for-token-launches-22ba89279f5f"
+                    target="_blank"
+                  >
+                    open letter from ConsenSys
+                  </a>&nbsp;that launched The Brooklyn Project.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-1 d-none d-md-block">
-            <div class="text-baseline" />
-          </div>
-          <div class="col-sm-11">
+      </div>
+    </section>
+    <section className="get-involved" id="get-involved">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8 col-lg-8 col-xl-8">
+            <h3>Get Involved</h3>
+            <h2>Want to join The Brooklyn Project?</h2>
             <p>
-              10 Principles that serve as general, aspirational guideposts for
-              projects.
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-1 d-none d-md-block">
-            <div class="text-baseline" />
-          </div>
-          <div class="col-sm-11">
-            <p>
-              Detailed suggested practices that form a foundation for
-              consumer-oriented behavior and community-driven collaboration.
-            </p>
-          </div>
-        </div>
-        <div class="row  mb-3">
-          <div class="col-sm-1 d-none d-md-block">
-            <div class="text-baseline" />
-          </div>
-          <div class="col-sm-11">
-            <p>
-              Recommended transparency goals that provide projects and their
-              communities a frame of reference for smart, useful transparency
-              practices.
-            </p>
-          </div>
-        </div>
-        <div class="row mb-5">
-          <div class="col-sm-12">
-            <p>
-              Crypto-economic systems form the backbone of blockchain
-              technology. In the context of the Core Framework, a
-              crypto-economic system will allow projects to apply to a registry
-              that (1) signals whether a project is implementing the framework,
-              and (2) invites the community to scrutinize their practices.
+              Provide comments, suggestions and feedback on the{" "}
+              <a href="http://bit.ly/CFTC-12-2018-RFC" target="_blank">
+                CFTC’s December 2018 Request for Input
+              </a>.
             </p>
             <p>
-              The goal of this initiative is to build a vibrant community that
-              collaborates with projects to solve their most urgent and
-              difficult problems, and a signaling system to inform consumers and
-              innovators when they may be treading into risky territory on
-              public blockchains.
+              Join other{" "}
+              <a href="https://collaborate.thebkp.com" target="_blank">
+                Open Collaborations
+              </a>.
             </p>
-          </div>
-        </div>
-        <div class="row mb-3">
-          <div class="col-sm-12">
             <p>
-              To collaborate with us, sign up
-              <Link to="signup"> here.</Link>
+              Join the discussion on{" "}
+              <a
+                href="https://t.me/joinchat/HRhhQEvAeC2t4wiYHquYUg"
+                target="_blank"
+              >
+                Telegram
+              </a>.&nbsp;
             </p>
+            <p>
+              Follow us on Twitter{" "}
+              <a href="https://twitter.com/TheBKP_Official" target="_blank">
+                @TheBKP_Official
+              </a>.
+            </p>
+            <p>
+              <a href="https://goo.gl/forms/PCSi9KEXJ8K2NZtH3" target="_blank">
+                Sign up here
+              </a>&nbsp;to get involved or stay updated.
+            </p>
+            <h3>{" "}</h3>
+            <h3>Resources</h3>
+            <ul>
+              <li>
+                <p>
+                  Our open letter announcing{" "}
+                  <a
+                    href="https://media.consensys.net/announcing-the-brooklyn-project-for-token-launches-22ba89279f5f"
+                    target="_blank"
+                  >
+                    The Brooklyn Project
+                  </a>.
+                </p>
+              </li>
+              <li>
+                <p>
+                  <a
+                    href="http://cardozo.yu.edu/sites/default/files/Cardozo%20Blockchain%20Project%20-%20Not%20So%20Fast%20-%20SAFT%20Response_final.pdf"
+                    target="_blank"
+                  >
+                    Not So Fast - Risks Related to the Use of a "SAFT" for Token
+                    Sales
+                  </a>
+                  <br />(A Cardozo Blockchain Project initiative, with support
+                  from ConsenSys).
+                </p>
+              </li>
+              <li>
+                <p>
+                  <a
+                    href="https://www.coinbase.com/legal/securities-law-framework.pdf"
+                    target="_blank"
+                  >
+                    A Securities Law Framework for Blockchain Tokens
+                  </a>
+                  <br />(An initiative of Coinbase, Coin Center, Union Square
+                  Ventures and Consensys).
+                </p>
+              </li>
+              <li>
+                <p>
+                  <a
+                    href="https://media.consensys.net/the-brooklyn-project-take-part-in-building-a-consumer-friendly-token-industry-bcb954c8e2ff"
+                    target="_blank"
+                  >
+                    The Brooklyn Project: Take Part in Building a
+                    Consumer-Friendly Token Industry
+                  </a>
+                </p>
+              </li>
+              <li>
+                <p>
+                  <a href="https://thebkp.com/token-taxonomy">
+                    Digital Asset Taxonomy
+                  </a>
+                </p>
+              </li>
+              <li>
+                <p>
+                  <a href="/crypto-asset-written-evidence">
+                    Crypto Asset Written Evidence for UK Parliament - 29 Apr
+                    2018
+                  </a>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Announcing The Brooklyn Project’s{" "}
+                  <a href="https://framework.thebkp.com" target="_blank">
+                    Consumer{" "}
+                  </a>
+                  <a
+                    href="https://media.consensys.net/a-consumer-token-framework-forging-a-path-towards-responsible-token-projects-7630c44538be"
+                    target="_blank"
+                  >
+                    Token
+                  </a>
+                  <a href="https://framework.thebkp.com" target="_blank">
+                    {" "}
+                    Framework
+                  </a>
+                </p>
+              </li>
+              <li>
+                <p>
+                  See many other resources on{" "}
+                  <a href="https://collaborate.thebkp.com" target="_blank">
+                    Open Collaboration Platform
+                  </a>.{" "}
+                </p>
+              </li>
+            </ul>
           </div>
+          <div className="col-12 col-md-4 col-lg-4 col-xl-4" />
         </div>
       </div>
     </section>
@@ -185,4 +255,7 @@ const mapState = state => ({ user: state.data.user });
 
 const actions = { logout };
 
-export default connect(mapState, actions)(Landing);
+export default connect(
+  mapState,
+  actions
+)(Landing);
