305637f044ffa77c0280d7879461ee064b7fe8ae
johnquiwa <johnquiwa@gmail.com>
2019-01-29 17:58:53 -0500

    link logo and home button to feed

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -51,7 +51,7 @@ class Navbar extends Component {
       <div className="header">
         <nav className="navbar navbar-expand-md no-gutters navbar--logo app-container">
           <div className="box--left">
-            <Link className="logo-header my-0 ml-0" to="/">
+            <Link className="logo-header my-0 ml-0" to="/feed">
               <img
                 width="100px"
                 height="auto"
@@ -67,7 +67,7 @@ class Navbar extends Component {
               ""
             )}
             {width > 600 ? (
-              <Link to="/" className="navbar__nav-item">
+              <Link to="/feed" className="navbar__nav-item">
                 home
               </Link>
             ) : (
