d7f6c7e4085cda7eb0c6f252f7cae38785d348c2
johnquiwa <johnquiwa@gmail.com>
2019-03-15 14:01:16 -0400

    Update feed item structure and css

--- client/components/FeedItem.js
+++ client/components/FeedItem.js
@@ -41,24 +41,25 @@ export default ({
       ) : null}
       <div className="feed-item__content">
         {category ? <h6>{category}</h6> : null}
-        <h5 className="mb-1">{title}</h5>
-        {tags && tags.length ? (
-          <div className="mb-2">
-            <span style={{ fontSize: "12px" }}>Tags: </span>
-            {tags.map(tag => (
-              <span key={`tag-${tag.name}`} className="badge badge-light">
-                {tag.name}
-                {"  "}
-              </span>
-            ))}
-          </div>
-        ) : (
-          ""
-        )}
+        <h5>{title}</h5>
+        <span className="feed-item__author-name d-flex mb-2">By {creatorName}</span>
+        {/*{tags && tags.length ? (*/}
+          {/*<div className="mb-2">*/}
+            {/*<span style={{ fontSize: "12px" }}>Tags: </span>*/}
+            {/*{tags.map(tag => (*/}
+              {/*<span key={`tag-${tag.name}`} className="badge badge-light">*/}
+                {/*{tag.name}*/}
+                {/*{"  "}*/}
+              {/*</span>*/}
+            {/*))}*/}
+          {/*</div>*/}
+        {/*) : (*/}
+          {/*""*/}
+        {/*)}*/}
         <div>{description}</div>
         <div className="feed-item__content-bottom d-flex justify-content-between align-items-center">
           <span>
-            {date} - {creatorName}
+            {date}
           </span>
           <div>
             <a className="contribution__action-btn">
