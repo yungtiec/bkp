47d17b4b6c7cba733d41572efbfa2c27c4481b38
yungtiec <tctammychu@gmail.com>
2019-04-05 16:30:32 +0800

    attempt to fix husky prepush check

--- library.webpack.config.js
+++ library.webpack.config.js
@@ -70,9 +70,9 @@ module.exports = {
               ]
             ],
             plugins: [
-              ["react-loadable/babel", {}],
-              ["@babel/plugin-proposal-class-properties", {}],
-              ["@babel/plugin-syntax-dynamic-import", {}]
+              "react-loadable/babel",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-syntax-dynamic-import"
             ]
           }
         }
